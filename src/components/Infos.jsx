208eab1803590f6c460a958915926923f7f1ff27
Nishat914 <jahannishat914@gmail.com>
2026-03-31 11:05:52 +0600

    adding premium digital tool

--- src/components/Infos.jsx
+++ src/components/Infos.jsx
@@ -10,10 +10,17 @@ const Infos = ({infoPromise}) => {
     const [selectedCart , setSelectedCart] = useState([])
     return (
         <>
-            <div className="flex items-center gap-3 justify-center my-4" >
-                
-                <button onClick={() => setSelectedType('Products')} className={`btn  ${selectedType === 'Products' ? "bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white" :  "btn btn-ghost"} rounded-2xl  ` }>Products</button>
+            <div className="flex flex-col items-center gap-3 justify-center mt-32 mb-4" >
+                <div className="text-center ">
+                    <h2 className="font-extrabold text-[#101727] text-5xl mb-4">Premium Digital Tools</h2>
+                    <p className="text-[#627382] mb-4">Choose from our curated collection of premium digital products designed <br />to boost your productivity and creativity.</p>
+                </div>
+                <div flex items-center gap-4 justify-center my-4>
+                    <button onClick={() => setSelectedType('Products')} className={`btn  ${selectedType === 'Products' ? "bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white" :  "btn btn-ghost"} rounded-2xl  ` }>Products</button>
                 <button onClick={() => setSelectedType('Cart')} className={`btn  ${selectedType === 'Cart' ? "bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white" :  "btn btn-ghost"} rounded-2xl  ` }>Cart ({selectedCart.length})</button>
+                </div>
+                
+                
             </div>
             
              {selectedType === "Products"? <AvailableInfos informations={informations} selectedCart={selectedCart} setSelectedCart={setSelectedCart} ></AvailableInfos> :
